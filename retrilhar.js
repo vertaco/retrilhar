cefaee91b5afab1ff7ee15104952aaf59ad64a3d
vertaco <156270207+vertaco@users.noreply.github.com>
2026-08-24 15:30:24 -0300

    Update retrilhar.js

--- retrilhar.js
+++ retrilhar.js
@@ -165,7 +165,7 @@ $(document).ready(function() {
 
     // Gatilho extra: dispara a função quando o campo do evento for alterado
     $(document).on('change', '[name="idEvento"]', function() {
-        setTimeout(checarVagasEAdicionarLink, 200);
+        setTimeout(checarVagasEAdicionarLink, 1000);
     });
 });
 
