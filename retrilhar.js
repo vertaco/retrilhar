bbd9d50fb3fefe1be9937022e747cae5db6a8496
vertaco <156270207+vertaco@users.noreply.github.com>
2026-09-01 21:48:44 -0300

    Update retrilhar.js

--- retrilhar.js
+++ retrilhar.js
@@ -211,7 +211,7 @@ function adicionarBadgesCards(){
         "Cachoeira do Dragão"
     ], function(index, atividade) {
         // Encontra a imagem da atividade baseada no texto alternativo ou título
-        $('img[alt="' + atividade + '"], img[title="' + atividade + '"]').each(function() {
+        $('.cover[alt="' + atividade + '"], .cover[title="' + atividade + '"]').each(function() {
             const $linkPai = $(this).parent();
             // Força o comportamento relativo no elemento pai
             $linkPai.addClass('position-relative d-block');
@@ -224,7 +224,7 @@ function adicionarBadgesCards(){
 	// Almoço incluso
 	$.each(["Cânion Chapada Imperial"], function(index, atividade) {
         // Encontra a imagem da atividade baseada no texto alternativo ou título
-        $('img[alt="' + atividade + '"], img[title="' + atividade + '"]').each(function() {
+        $('.cover[alt="' + atividade + '"], .cover[title="' + atividade + '"]').each(function() {
             const $linkPai = $(this).parent();
             // Força o comportamento relativo no elemento pai
             $linkPai.addClass('position-relative d-block');
@@ -237,7 +237,7 @@ function adicionarBadgesCards(){
 	// Transporte incluso
 	$.each(["Excursão Terra Ronca-GO [10 a 12/10]","Excursão Mambaí-GO [05 a 07/09]"], function(index, atividade) {
         // Encontra a imagem da atividade baseada no texto alternativo ou título
-        $('img[alt="' + atividade + '"], img[title="' + atividade + '"]').each(function() {
+        $('.cover[alt="' + atividade + '"], .cover[title="' + atividade + '"]').each(function() {
             const $linkPai = $(this).parent();
             // Força o comportamento relativo no elemento pai
             $linkPai.addClass('position-relative d-block');
